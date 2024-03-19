197560f91ed6feb1d13738cba96d097242ebb08d
AndoNando15 <andonando158@gmail.com>
2024-03-20 03:30:49 +0700

    update page bar

---
+++ database/migrations/2024_03_19_110137_create_buildings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('location');
+            $table->string('image');
+            $table->float('lowest_price');
+            $table->float('highest_price');
+            $table->text('description');
+            $table->float('land_area');
+            $table->float('building_area');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buildings');
+    }
+};
